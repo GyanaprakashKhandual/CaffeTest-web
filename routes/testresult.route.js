1c9c778749534be44c758fa1e13bcc670f5b8014
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-08-31 11:07:39 +0530

    Route changes

--- routes/testresult.route.js
+++ routes/testresult.route.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { storeTestResults } = require("../controllers/testresult.controller");
-const { protect } = require("../middlewares/auth.middleware");
+const protect = require("../middlewares/auth.middleware");
+
 
 // POST /api/test-results
 router.post("/", protect, storeTestResults);
